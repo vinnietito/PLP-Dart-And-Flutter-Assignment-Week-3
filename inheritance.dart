ed59baf746f87032d3e779bd1c0e48f65afcff68
vinnietito <vincentnjuguna69@gmail.com>
2024-11-16 19:45:33 +0300

    Commits

---
+++ inheritance.dart
@@ -0,0 +1,22 @@
+//Superclass
+class Animal {
+  void speak(){
+    print("The animal makes a sound");
+  }
+}
+
+//Subclass inheriting from Animal
+class Dog extends Animal {
+  @override
+  void speak() {
+    print("The dog barks");
+  }
+}
+
+void main(){
+  var animal = Animal();
+  animal.speak();//Outputs: The animal makes a sound
+
+  var dog = Dog();
+  dog.speak(); //Outputs: The dog barks
+}
