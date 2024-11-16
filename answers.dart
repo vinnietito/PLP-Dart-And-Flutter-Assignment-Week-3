7006dd13ac72b581bd0873a39cc04ffa618960b2
vinnietito <vincentnjuguna69@gmail.com>
2024-11-16 20:13:23 +0300

    Commits

--- answers.dart
+++ answers.dart
@@ -103,3 +103,42 @@ void main() {
   Shape square = Square();
   square.draw(); //Outputs: Drawing a square
 }
+
+// 4. Abstraction
+
+//Abstract class
+abstract class Animal {
+  //Abstract method (no implementation)
+  void sound();
+
+  //Regular method
+  void eat(){
+    print("This animal eats food");
+  }
+}
+
+//Subclass 1
+class Dog extends Animal {
+  @override
+  void sound() {
+    print("The dog barks");
+  }
+}
+
+//Subclass 2
+class Cat extends Animal {
+  @override
+  void sound() {
+    print("The cat meows");
+  }
+}
+
+void main() {
+  var dog = Dog();
+  dog.sound(); //Outputs: The dog barks
+  dog.eat(); //Outputs: This animal eats food
+
+  var cat = Cat();
+  cat.sound();  //Outputs: The cat meows
+  cat.eat(); //Outputs: this animal eats food
+}
