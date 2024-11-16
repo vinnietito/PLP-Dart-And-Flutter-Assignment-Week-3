88eace37dffe4a4927790d90326fd96e58cea75e
vinnietito <vincentnjuguna69@gmail.com>
2024-11-16 19:58:02 +0300

    Commits

---
+++ polymorphism.dart
@@ -0,0 +1,32 @@
+// 3. Polymorphism
+
+//Superclass
+class Shape {
+  void draw(){
+    print("Drawing a shape");
+  }
+}
+
+//Subclass 1
+class Circle extends Shape {
+  @override
+  void draw() {
+    print("Srawing a circle");
+  }
+}
+
+//Subclass 2
+class Square extends Shape {
+  @override
+  void draw() {
+    print("Drawing a square");
+  }
+}
+
+void main() {
+  Shape shape = Shape();
+  shape.draw(); //Outputs: Drawing a shape
+
+  Shape circle = Circle();
+  circle.draw();
+}
