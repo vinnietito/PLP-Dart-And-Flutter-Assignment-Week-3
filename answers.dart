1788ffcf8acb98e7ceebce6bb640e88b5a11f167
vinnietito <vincentnjuguna69@gmail.com>
2024-11-16 19:33:40 +0300

    Commits

--- answers.dart
+++ answers.dart
@@ -14,3 +14,26 @@ class Employee {
 
   // Setter for name
   set name(String name) => _name = name;
+
+  //Getter for salary
+  double get salary => _salary;
+
+  //Setter for salary (with validation)
+  set salary(double salary) {
+    if (salary > 0) {
+      _salary = salary;
+    } else {
+      print("Salary must be positive");
+    }
+  }
+}
+
+void main(){
+  var employee = Employee("Vincent Kimanthi", 100000);
+  print("Name: ${employee.name}");
+  print("Salary: ${employee.salary}");
+
+  //Changing data using setters
+  employee.name = "Kimanthi Vincent";
+  employee.salary = 65000;
+}
